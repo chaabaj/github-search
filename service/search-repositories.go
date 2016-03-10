58bbefc1f64511de0fee55a104697b5d78d7a68f
jalal <jalal@debian>
2016-03-10 02:04:01 +0100

    Update token used to access to public repositories

--- service/search-repositories.go
+++ service/search-repositories.go
@@ -16,7 +16,7 @@ type searchResult struct {
 
 // github api definition with a valid auth token
 // It provide only access to public repository
-var githubApi = api.New("https://api.github.com", "token da7f8a6aee3763253a95e366300e5b4054ecfe5b")
+var githubApi = api.New("https://api.github.com", "token bf107c4ecf091b0f09f5eb3e4fb77432589f5b21")
 
 // Try to get the languages used in the repository
 func getRepositoryLanguages(repo *datas.Repository) (map[string]interface{}, error) {
